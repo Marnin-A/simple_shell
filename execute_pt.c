1166961fec5322d1046d8a8597bb53c6ddbed33d
Elsie Funkor <elsiefunk12@gmail.com>
2023-10-17 16:14:52 -0700

    too many

--- execute_pt.c
+++ execute_pt.c
@@ -17,48 +17,50 @@ void execute_pt(const char *uput)
 
 	if (child_pid == -1)
 	{
-		perror("fork was unsuccessful");
-		exit(EXIT_FAILURE);
+	perror("fork was unsuccessful");
+	exit(EXIT_FAILURE);
 	}
 	else if (child_pid == 0)
 	{
-		ux_commandz1 = malloc(128 * sizeof(char *));
-		if (ux_commandz1 == NULL)
-		{
-			perror("memory allocation was unsuccessful");
-			exit(EXIT_FAILURE);
-		}
+	ux_commandz1 = malloc(128 * sizeof(char *));
+	if (ux_commandz1 == NULL)
+	{
+	perror("memory allocation was unsuccessful");
+	exit(EXIT_FAILURE);
+	}
 
-		token = strtok((char *)uput, " ");
-		while (token != NULL)
-		{
-			ux_commandz1[w] = token;
-			token = strtok(NULL, " ");
-			w++;
-		}
-		ux_commandz1[w] = NULL;
+	token = strtok((char *)uput, " ");
+	while (token != NULL)
+	{
+	ux_commandz1[w] = token;
+	token = strtok(NULL, " ");
+	w++;
+	}
+	ux_commandz1[w] = NULL;
 
-		exec = ux_commandz1[0];
-		cd_path = "/bin/";
-		cd_path = malloc(strlen(cd_path) + strlen(exec) + 1);
-		if (cd_path == NULL)
-		{
-			perror("memory allocation was unsuccessful");
-			exit(EXIT_FAILURE);
-		}
-		strcpy(cd_path, "/usr/bin/");
-		strcat(cd_path, exec);
+	exec = ux_commandz1[0];
+	cd_path = "/bin/";
+	cd_path = malloc(strlen(cd_path) + strlen(exec) + 1);
+	if (cd_path == NULL)
+	{
+	perror("memory allocation was unsuccessful");
+	exit(EXIT_FAILURE);
+	}
+	strcpy(cd_path, "/usr/bin/");
+	strcat(cd_path, exec);
 
-		if (execve(cd_path, ux_commandz1, env) == -1)
-		{
-			perror("Command execution was unsuccessful");
-			exit(EXIT_FAILURE);
-		}
-		free(cd_path);
-		free(ux_commandz1);
+	if (execve(cd_path, ux_commandz1, env) == -1)
+	{
+	perror("Command execution was unsuccessful");
+	exit(EXIT_FAILURE);
+	}
+
+	free(cd_path);
+	free(ux_commandz1);
 	}
 	else
 	{
-		waitpid(child_pid, &arCd, 0);
+	waitpid(child_pid, &arCd, 0);
 	}
 }
+
